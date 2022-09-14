f0e5cd78f896291a937ca7e9c2dc01bc59a78b35
Gympast <112376007+Gympast@users.noreply.github.com>
2022-09-14 15:13:59 +0200

    facit

--- Lab1/Lab1/Program.cs
+++ Lab1/Lab1/Program.cs
@@ -1,2 +1,31 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string input = "29535123p48723487597645723645";
+//string input = "29535123p48";
+long sum = 0;
+
+for (int i = 0; i < input.Length; i++)
+{
+    for (int j = i + 1; j < input.Length; j++)
+    {
+        if (Char.IsDigit(input[i]))
+        {
+            
+            if (input[i] == input[j])
+            {
+                Console.Write(input.Substring(0, i));
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.Write(input.Substring(i, j - i + 1));
+                Console.ForegroundColor = ConsoleColor.Gray;
+                Console.WriteLine(input.Substring(j + 1));
+                sum += Convert.ToInt64(input.Substring(i, j - i + 1));
+                break;
+            }
+            else if (Char.IsLetter(input[j]))
+            {
+                break;
+            }
+        }
+    }
+
+}
+
+Console.WriteLine($"\nSumman är {sum}");
