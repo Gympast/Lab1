8e7ee4d670c2623a73d12b527be8ecb1b8880408
Gympast <112376007+Gympast@users.noreply.github.com>
2023-09-04 14:54:41 +0200

    Add project files.

---
+++ Lab1/Program.cs
@@ -0,0 +1,31 @@
+﻿string input = "29535123p48723487597645723645";
+long sum = 0;
+Console.WriteLine(input);
+Console.WriteLine();
+
+for (int i = 0; i < input.Length; i++)
+{
+    for (int j = i + 1; j < input.Length; j++)
+    {
+        if (Char.IsDigit(input[j]))
+        {
+            if (input[i] == input[j])
+            {
+                Console.ForegroundColor = ConsoleColor.Gray;
+                Console.Write(input.Substring(0, i));
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.Write(input.Substring(i, j - i + 1));
+                Console.ForegroundColor = ConsoleColor.Gray;
+                Console.Write(input.Substring(j + 1));
+                Console.WriteLine();
+                sum += Convert.ToInt64(input.Substring(i, j - i + 1));
+                break;
+            }
+        }
+        else
+        {
+            break;
+        }
+    }
+}
+Console.WriteLine(sum);
